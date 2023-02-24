45d40fae85c90eedb86429428bae0b8c26e0f172
priyankasaini-TPG <priyanka.saini@3pillarglobal.com>
2023-02-24 23:59:53 +0530

    save & cancel button

--- Assignment1/src/app/infopage/form/form.component.ts
+++ Assignment1/src/app/infopage/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+
+  Name: string  = "";
+  CName: string = "";
+  defaultDegree: string = 'B.Tech';
+  Specialization: string = "";
+
+  @ViewChild('myform') form?: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+  }
+
+  clearAll(){
+    this.Name = "";
+    this.CName = "";
+    this.defaultDegree = "";
+    this.Specialization = "";
+  }
 
 }
